be120caca8ceba9b8cc6d9579ca4039b037fc94e
Nikolay Bonev <nikolay@whale-agency.com>
2020-09-22 17:58:20 +0200

    first commit

---
+++ frontity.settings.js
@@ -0,0 +1,42 @@
+const settings = {
+  name: "test-cpt",
+  state: {
+    frontity: {
+      url: "https://test.frontity.org",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development",
+    },
+  },
+  packages: [
+    {
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
+            ["About Us", "/about-us/"],
+          ],
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "https://api.koenighaus-heizsysteme.de/wp-json",
+        },
+      },
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
+
+export default settings;
